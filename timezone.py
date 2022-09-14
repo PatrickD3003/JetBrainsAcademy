fe9e002b099280de861522c01f9af078f05ae021
AD <AD@LLs-MacBook-Air.local>
2022-09-14 23:53:49 +0700

    remove symbols

--- timezone.py
+++ timezone.py
@@ -1,31 +1,31 @@
+# ceritanya patokan awal itu timezone london, jem 10:30 Tuesday
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 symbols = ["+", "-"]
 i = 1
 hour_to_minute = 60
-london_hour = 10
-london_minute = 30
+london_hour, london_minute = 10, 30
 now_time = london_hour * hour_to_minute + london_minute
 one_day = 24 * hour_to_minute
 
-change_time = input()
-print(now_time)
+change_time = int(input())
 
-if change_time[0] == symbols[0]:
-    now_time += int(change_time[1:]) * hour_to_minute
-    if now_time > one_day:
-        i += 1
-        print(days[i])
-    else:
-        print(days[i])
+now_time += change_time * hour_to_minute
 
-elif change_time[0] == symbols[1]:
-    now_time -= int(change_time[1:]) * hour_to_minute
-    if now_time < 0:
-        i -= 1
-        print(days[i])
-    else:
-        print(days[i])
-elif int(change_time) == 0:
-    print(days[i])
+def text(d, i, n, h):
+    print(f"it is {d[i]}, {n // h}:{n % h} in this timezone right now")
 
-print(now_time)
+
+
+if now_time > one_day:
+    i += 1
+    if i > len(days)-1:
+        i = 0
+    now_time -= one_day
+    text(days, i, now_time, hour_to_minute)
+
+elif now_time < 0:
+    i -= 1
+    now_time += one_day
+    text(days, i, now_time, hour_to_minute)
+else:
+    text(days, i, now_time, hour_to_minute)
