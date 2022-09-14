463d820c2a8665661bfdc6878d45900826d732fc
AD <AD@LLs-MacBook-Air.local>
2022-09-14 23:36:00 +0700

    add a note

--- timezoneClass.py
+++ timezoneClass.py
@@ -10,14 +10,17 @@ class TimeZone:
         self.change_time = 0
         self.now_time = self.hour * self.hour_to_minute + self.minute
 
-    def change_timezone(self):
+    def change_timezone(self):         
+        # cara inputnya, symbol+angka, cth = +12 , -11 dll 
         self.change_time = int(input())
         self.now_time += self.change_time * self.hour_to_minute
 
         if self.now_time > self.one_day:
             self.number_day += 1
+            
             if self.number_day > len(self.days)-1:
                 self.number_day = 0
+
             print(self.days[self.number_day])
             self.now_time -= self.one_day
 
@@ -33,7 +36,6 @@ class TimeZone:
         return f"it is {self.days[self.number_day]}, {self.now_time // self.hour_to_minute}:{self.now_time % self.hour_to_minute} in this timezone right now"
 
             
-
 london = TimeZone(TimeZone.days[TimeZone.number_day], 10, 30)
 london.change_timezone()
 print(london.current_time())
